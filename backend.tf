fac10155b0a8ed105d51974384e06dca5362e539
toussaintvla <48733500+toussaintvla@users.noreply.github.com>
2020-03-11 12:18:21 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#    backend "s3" {
-#      bucket   = "vladimir-toussaint"
-#      key      = "s3/terraform-s3.tfstate"
-#      region   = "us-east-1"
-#     # role_arn = ""
-#    }
-# }
+terraform {
+   backend "s3" {
+     bucket   = "vladimir-toussaint"
+     key      = "s3/terraform-s3.tfstate"
+     region   = "us-east-1"
+    # role_arn = ""
+   }
+}
